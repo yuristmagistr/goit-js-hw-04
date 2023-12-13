5520c051aa4b519d9de7676799f857ec670a2422
yuristmagistr <144477228+yuristmagistr@users.noreply.github.com>
2023-12-13 22:37:41 +0100

    start-finish

--- js/task-3.js
+++ js/task-3.js
@@ -1,16 +1,24 @@
-function filterArray(numbers, value) {
-    const newArray = [];
-    for (const number of numbers) {
-        if (value < (number)) {
-            newArray.push(number);
-        }
-    }
-    return newArray;
-}
+const profile = {
+    username: "Jacob",
+    playTime: 300,
+
+    changeUsername: function (newName) {
+    this.username = newName;
+    },
+
+    updatePlayTime: function (hours) {
+    this.playTime += hours;
+},
+    getInfo: function () {
+        return `${this.username} has ${this.playTime} active hours!`;
+    },
+};
 
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
